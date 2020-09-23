740980f1a4ac8d41ab5b05870e160ff9a4ad9d4d
Miguel <eduardo.quintos23@gmail.com>
2020-09-23 20:26:14 +0800

    revise the deploy

--- deploy.php
+++ deploy.php
@@ -1,8 +1,8 @@
 <?php
 // Repository Update
-shell_exec('/usr/local/cpanel/bin/uapi VersionControl update name=repo_name repository_root=/home/webapi/public_html/api branch=master source_repository=\'{"remote_name":"origin"}\'
+shell_exec('/usr/local/cpanel/bin/uapi VersionControl update name=repo_name repository_root=/home/webapi/public_html branch=master source_repository=\'{"remote_name":"origin"}\'
 ');
 // Deploy
-shell_exec('/usr/local/cpanel/bin/uapi VersionControlDeployment create repository_root=/home/webapi/public_html/api');
+shell_exec('/usr/local/cpanel/bin/uapi VersionControlDeployment create repository_root=/home/webapi/public_html');
 echo "done";
 ?>
