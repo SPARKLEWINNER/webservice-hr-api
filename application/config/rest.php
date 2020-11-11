b27b47b6992b9a0d931a1c9f74b0dfb1eed9c6a0
Miguel <eduardo.quintos23@gmail.com>
2020-11-12 01:38:39 +0800

    added cors

--- application/config/rest.php
+++ application/config/rest.php
@@ -557,4 +557,4 @@ $config['allow_any_cors_domain'] = TRUE;
 | e.g. $config['allowed_origins'] = ['http://www.example.com', 'https://spa.example.com']
 |
 */
-$config['allowed_cors_origins'] = [];
+$config['allowed_cors_origins'] = ['http://portal.7star.com.ph','http://portal.syzygy.com.ph'];
