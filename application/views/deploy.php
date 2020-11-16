f9a8920c7a85e8feeb6d8a30588ebc637ab9ccb4
Miguel <eduardo.quintos23@gmail.com>
2020-11-17 05:15:19 +0800

    CI/CD dev deployment settings adjst

--- application/views/deploy.php
+++ application/views/deploy.php
@@ -1,8 +1,8 @@
 <?php
 // Repository Update
-shell_exec('/usr/local/cpanel/bin/uapi VersionControl update name=repo_name repository_root=/home/webapi/public_html branch=master source_repository=\'{"remote_name":"origin"}\'
+shell_exec('/usr/local/cpanel/bin/uapi VersionControl update name=dev_repo repository_root=/home/webapi/public_html/staging branch=dev source_repository=\'{"remote_name":"origin"}\'
 ');
 // Deploy
-shell_exec('/usr/local/cpanel/bin/uapi VersionControlDeployment create repository_root=/home/webapi/public_html');
+shell_exec('/usr/local/cpanel/bin/uapi VersionControlDeployment create repository_root=/home/webapi/public_html/staging');
 echo "done";
 ?>
