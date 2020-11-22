0919729f01532181dba324a5c1939b269121b84d
Miguel <eduardo.quintos23@gmail.com>
2020-11-23 00:07:54 +0800

    remove die dump

--- application/models/Main_mdl.php
+++ application/models/Main_mdl.php
@@ -412,8 +412,6 @@ class Main_mdl extends Base_Model {
     public function record_pool_weeks_pull($company, $weeks){
 
         $query = "SELECT * FROM `applications` where `company` = '{$company}' AND date_created < now() - interval {$weeks} week OR status = 0"; 
-        echo $query;
-        die();
         $result = $this->db->query($query);
         return ($result->num_rows() > 0) ? $result->result_array() : false;
 
