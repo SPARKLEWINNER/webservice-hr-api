99b989bb8597fbbb9c676ca58be2aec04d99f2b9
Miguel Quintos <miguel@taal.com>
2021-04-19 14:10:58 +0800

    MASTER: added patch method for controller

--- application/libraries/Base_Controller.php
+++ application/libraries/Base_Controller.php
@@ -5,7 +5,7 @@ require APPPATH . '/libraries/REST_Controller.php';
 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE, PATCH");
 header('Content-Type: application/json');
 header('Content-Type: multipart/form-data');
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
