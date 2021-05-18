bf01a6a86b7e3ec94dd8170452785ba106e73f96
Miguel Quintos <miguel@taal.com>
2021-05-18 15:18:47 +0800

    MAIN: adjusted exam log display to last 40 days

--- application/models/Main_mdl.php
+++ application/models/Main_mdl.php
@@ -929,7 +929,7 @@ class Main_mdl extends Base_Model
     public function record_exam_logs_pull($company)
     {
 
-        $applicants = "SELECT * FROM `applications` WHERE `date_created` >= now()-interval 2 month AND  `company` = '{$company}' ORDER BY `id`";
+        $applicants = "SELECT * FROM `applications` WHERE `date_created` >= now()-interval 40 day AND `company` = '{$company}' ORDER BY `id`";
         $result = $this->db->query($applicants);
         if ($result->num_rows() > 0) {
             $app_res = $result->result_array();
